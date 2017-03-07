52012ea2025cdacf059df20154e913c63cb3f19a
Shi Yin <joysword@me.com>
2017-03-07 16:10:20 -0600

    added google translate code to the result page

--- rnn_tang_poetry.py
+++ rnn_tang_poetry.py
@@ -91,6 +91,11 @@ def show_result(id):
         </nav>
 
                 <div class="container">
+                <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'zh-CN', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                     <div class="row">
                         <table class="table">
         '''
