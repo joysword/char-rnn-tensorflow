2127f955b020845786f5ef71327a88b3cf913a50
Shi Yin <joysword@me.com>
2017-05-06 01:37:29 -0500

    I don’t remember if this is changing from tf1.0 to tf0.12 or the opposite

--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 import tensorflow as tf
-from tensorflow.python.ops import rnn_cell
-from tensorflow.python.ops import seq2seq
+# from tensorflow.python.ops import rnn_cell
+# from tensorflow.python.ops import seq2seq
+from tensorflow.contrib import rnn as rnn_cell
+from tensorflow.contrib import legacy_seq2seq as seq2seq
+
 
 import numpy as np
 
@@ -35,7 +38,8 @@ class Model():
             softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
             with tf.device("/cpu:0"):
                 embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-                inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
+                # inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
+                inputs = tf.split(tf.nn.embedding_lookup(embedding, self.input_data), args.seq_length, 1)
                 inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
         def loop(prev, _):
@@ -44,7 +48,7 @@ class Model():
             return tf.nn.embedding_lookup(embedding, prev_symbol)
 
         outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
-        output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
+        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
         self.logits = tf.matmul(output, softmax_w) + softmax_b
         self.probs = tf.nn.softmax(self.logits)
         loss = seq2seq.sequence_loss_by_example([self.logits],
